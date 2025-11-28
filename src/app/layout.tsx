36809480a4af1bf9084d3c1f482e1d5c39f6a73c
Riccardo Marconato <riccardomarconato@MacBook-Pro-di-Riccardo.local>
2025-11-28 14:08:52 +0100

    Fix: Remove missing font references

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,20 +1,8 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
 import "./globals.css";
 import { KleverProvider } from "@/context/KleverContext";
 
 export const dynamic = 'force-dynamic';
-
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
 
 export const metadata: Metadata = {
   title: "Digiko - DeFi Ecosystem on Klever Blockchain",
@@ -28,9 +16,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className="antialiased">
         <KleverProvider>
           {children}
         </KleverProvider>
